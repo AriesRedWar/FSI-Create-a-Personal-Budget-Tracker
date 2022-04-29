03c8f78efd402fd2c541ac88146adc87bcf75f9e
AriesRedWar <philipjbowman5@gmail.com>
2022-04-28 20:44:07 -0400

    weekly in reverse

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+for (let i = weeklyExpenseQuestions.length-1; i >= 0 ; i--) {
     weeklyExpenses += parseFloat(window.prompt(weeklyExpenseQuestions[i]))
-}
+} //Questions in reverse order when pops up
 
 for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
     monthlyExpenses += parseFloat(window.prompt(monthlyExpenseQuestions[i]))
-}
+} //Questions in Normal order
 
 for (let i = 0; i < annualExpenseQuestions.length; i++) {
     annualExpenses += parseFloat(window.prompt(annualExpenseQuestions[i]))
-}
-// New Budget
-
-
+} // New Budget
